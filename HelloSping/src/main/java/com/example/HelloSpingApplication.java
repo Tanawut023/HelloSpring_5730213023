1daf537592891dd4b70011bfea5100a3fdb34fac
Tanawut Rungpetnimit <tanawut913@gmail.com>
2016-11-21 21:45:18 +0700

    add spring

---
+++ HelloSping/src/main/java/com/example/HelloSpingApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpingApplication.class, args);
+	}
+}
